0c38603869fc728b0865ce791cef6925af830952
goshipcode <goshipcode@gmail.com>
2023-04-01 14:13:28 -0500

    add docs changes and polish up project

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # brain-cli
+
+This is a rust cli (command line interface) program I developed to let users save things they need to remember later.
+
+This is the first v1 `very bare bones version` that:
+1. creates a brain.md file where the executable is run
+2. Stores your remember sakes in that file
+3. Has the ability to list your memories
+
+# Why?
+I find there are many things I want to remember, but not an easy way to just dump them somewhere formatted nicely. 
+So this is an attempt at just dumping things I need to refer back to later. I plan to make iterations on this idea
+and add more features. 
+
+For now just a simple project I have developed to help me learn rust better.
+
+# What it looks like:
+![](./brainv1.gif)
+
+## Installation
+**Prereq:** Need to have rust/cargo installed [instructions here](https://doc.rust-lang.org/book/ch01-01-installation.html)
+
+```
+git clone https://github.com/goshipcode/brain-cli.git
+cd brain-cli
+cargo run 
+```
+
+## How to add this binary to your path in macos and linux
+```
+# first create a release version of the binary
+cargo build --release
+
+# Now copy the binary to 
+cp target/release/brain [directory of your choice]/bin
+chmod 755 [directory of your choice]/bin/brain
+
+# Add the binary to your path
+export PATH="[directory of your choice]/bin:$PATH"
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
+use std::io;
+use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
+use std::fs::OpenOptions;
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    //dbg!(args);
+
+    if args.len() < 2 {
+        save_memory();
+    } else {
+        let first_arg = &args[1];
+        if first_arg.eq("list") {
+            //list the brain here
+            println!("🧠 (Brain): Fetching all memories.....");
+            let brain_file_exists = Path::new("./brain.md").exists();
+            if !brain_file_exists {
+                println!("🧠 (Brain): invalid command, please try again");
+                std::process::abort();
+            }
+
+            let file_contents = fs::read_to_string("./brain.md")
+                .expect("error occurred, could not read brain file");
+
+            println!("🧠💡🧠💡🧠💡🧠💡🧠💡🧠💡🧠💡🧠💡🧠\n{file_contents}");
+        } else {
+            println!("🧠 (Brain): invalid command, please try again");
+            std::process::abort();
+        }
+    }
+}
+
+fn save_memory() {
+    println!("🧠 (Brain): What do you want to remember?");
+
+    let mut thing_to_remember = String::new();
+
+    io::stdin()
+        .read_line(&mut thing_to_remember)
+        .expect("Failed to read your request. Please try again"); //this will crash the program if error happens
+
+    let brain_file_exists = Path::new("./brain.md").exists();
+
+    if !brain_file_exists { //need to create the file, the write to it
+        File::create("./brain.md").expect("Failed to create file, please try again");
+    }
+
+    // get next input on why to store it, and then save it
+    println!("🧠 (Brain): Enter Title of this memory:");
+
+    let mut why_store_this_thing = String::new();
+    io::stdin()
+        .read_line(&mut why_store_this_thing)
+        .expect("Failed to read your request. Please try again");
+
+    let mut md_file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("./brain.md")
+        .unwrap();
+
+    if !brain_file_exists {
+        writeln!(md_file,"# Your 🧠 Brain File").expect("Failed to write to file, please try again");
+    }
+
+    //now save two lines in the file.
+    writeln!(md_file,"## {}", why_store_this_thing).expect("Failed to write to file, please try again");
+    writeln!(md_file,"```\n{}```\n", thing_to_remember).expect("Failed to write to file, please try again");
+
+    println!("🧠 (Brain): I've saved the new item.\n Hint: use `brain list` to open all my memories");
 }
