52c5c9642cecca8814d33c84591d57e9cd1bd9e8
goshipcode <goshipcode@gmail.com>
2023-04-05 20:19:40 -0500

    Add v2 of brain cli

--- README.md
+++ README.md
@@ -1,18 +1,32 @@
 # brain-cli
 
-This is a rust cli (command line interface) program I developed to let users save things they need to remember later.
+This is a rust cli (command line interface) program I developed to let users save things they need to reference later.
 
-This is the first v1 `very bare bones version` that:
-1. creates a brain.md file where the executable is run
-2. Stores your remember sakes in that file
-3. Has the ability to list your memories
+This is the 🥈`second (v2)` less bare bones version.
+## Usage/Features:
+Invoke the Brain with `brain`
+```
+brain
+🧠 (Brain): What do you want to remember?
+....{your response}..
+```
+^ This saves the contents to `$HOME/.brain-cli/brain.md` or if `$HOME` does not exist, it saves it to the `./.brain-cli/brain.md` current dir
+
+Other commands:
+```
+# list the brain file in the terminal (with some minimal markdown highlighting)
+brain list
+
+# print out the location of the brain file on your computer
+brain location
+
+```
+
+
 
 # Why?
-I find there are many things I want to remember, but not an easy way to just dump them somewhere formatted nicely. 
-So this is an attempt at just dumping things I need to refer back to later. I plan to make iterations on this idea
-and add more features. 
-
-For now just a simple project I have developed to help me learn rust better.
+I find there are many things I want to remember, but not a super easy way to just dump them somewhere formatted nicely. 
+So this is an attempt at just dumping things I need to refer back to later.
 
 # What it looks like:
 ![](./brainv1.gif)
@@ -28,14 +42,17 @@ cargo run
 
 ## How to add this binary to your path in macos and linux
 ```
-# first create a release version of the binary
+# First create a release version of the binary
 cargo build --release
 
-# Now copy the binary to 
+# Now copy the binary to a bin directory of your choice:
 cp target/release/brain [directory of your choice]/bin
 chmod 755 [directory of your choice]/bin/brain
 
 # Add the binary to your path
 export PATH="[directory of your choice]/bin:$PATH"
+
+# Now you can execute `brain` from your terminal:
+brain 
 ```
 
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,42 @@
 use std::io;
 use std::path::Path;
 use std::fs::File;
-use std::io::prelude::*;
 use std::fs::OpenOptions;
 use std::env;
 use std::fs;
+use termimad::{ MadSkin, rgb};
+use std::io::Write;
+
+fn make_skin() -> MadSkin {
+    let mut skin = MadSkin::default();
+    skin.set_headers_fg(rgb(246, 189, 0));
+    skin
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    //dbg!(args);
-
+    let brain_path = get_brain_path();
     if args.len() < 2 {
         save_memory();
     } else {
         let first_arg = &args[1];
         if first_arg.eq("list") {
-            //list the brain here
-            println!("🧠 (Brain): Fetching all memories.....");
-            let brain_file_exists = Path::new("./brain.md").exists();
+            // list the brain here
+            let brain_file_exists = Path::new(&brain_path).exists();
             if !brain_file_exists {
                 println!("🧠 (Brain): invalid command, please try again");
                 std::process::abort();
             }
 
-            let file_contents = fs::read_to_string("./brain.md")
+            let file_contents = fs::read_to_string(&brain_path)
                 .expect("error occurred, could not read brain file");
 
-            println!("🧠💡🧠💡🧠💡🧠💡🧠💡🧠💡🧠💡🧠💡🧠\n{file_contents}");
+            println!("🧠💡 Brain Contents Below 🧠💡");
+            let skin = make_skin();
+            skin.print_text(file_contents.as_str());
+
+        } else if first_arg.eq("location") {
+            println!("🧠 (Brain): Here is my full file path: {brain_path}");
         } else {
             println!("🧠 (Brain): invalid command, please try again");
             std::process::abort();
@@ -34,20 +44,32 @@ fn main() {
     }
 }
 
-fn save_memory() {
-    println!("🧠 (Brain): What do you want to remember?");
+fn get_brain_path() -> String {
+    let optional_value = option_env!("HOME");
+    let mut base_dir = optional_value.unwrap_or(".").to_string();
+    base_dir.push_str("/.brain-cli/brain.md");
+    base_dir
+}
 
+fn save_memory() {
+    let file_location = get_brain_path();
+    let brain_file_exists = Path::new(file_location.as_str()).exists();
+
+    println!("brain file exists? {brain_file_exists}, file location {file_location}");
+
+    if !brain_file_exists { //need to create the file, the write to it
+        let path = std::path::Path::new(&file_location);
+        let prefix = path.parent().unwrap();
+        std::fs::create_dir_all(prefix).unwrap();
+        File::create(&file_location).expect("Failed to create file, please try again");
+    }
+
+    println!("🧠 (Brain): What do you want to remember?");
     let mut thing_to_remember = String::new();
 
     io::stdin()
         .read_line(&mut thing_to_remember)
         .expect("Failed to read your request. Please try again"); //this will crash the program if error happens
-
-    let brain_file_exists = Path::new("./brain.md").exists();
-
-    if !brain_file_exists { //need to create the file, the write to it
-        File::create("./brain.md").expect("Failed to create file, please try again");
-    }
 
     // get next input on why to store it, and then save it
     println!("🧠 (Brain): Enter Title of this memory:");
@@ -60,7 +82,7 @@ fn save_memory() {
     let mut md_file = OpenOptions::new()
         .write(true)
         .append(true)
-        .open("./brain.md")
+        .open(&file_location)
         .unwrap();
 
     if !brain_file_exists {
